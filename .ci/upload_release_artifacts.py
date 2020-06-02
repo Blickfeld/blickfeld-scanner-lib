004550221ad75b102261f7a0768515a7484d7486
Johannes Meßmer <johannes.messmer@blickfeld.com>
2020-06-02 17:52:19 +0200

    CI: Automatically create GitHub release

---
+++ .ci/upload_release_artifacts.py
@@ -0,0 +1,54 @@
+import os
+import requests
+from collections import namedtuple
+
+from github import Github, GitRelease
+from git import Repo
+from fetch_artifacts import fetch_artifacts
+
+local_repo = Repo()
+
+g = Github(os.environ['GITHUB_TOKEN'])
+remote_repo = g.get_repo("Blickfeld/blickfeld-scanner-lib")
+
+# Get latest tag
+release_tag = local_repo.git.describe(abbrev=0)
+
+# CreateOrFetch release from GitHub API
+try:
+    release = remote_repo.get_release(release_tag)
+except:
+    print("Create release for", release_tag)
+    headers, data = remote_repo._requester.requestJsonAndCheck("POST", remote_repo.url + "/releases", input = {
+        "tag_name": release_tag,
+        "prerelease": True,
+    })
+    
+    release = GitRelease.GitRelease(remote_repo._requester, headers, data, completed=True)
+    
+# Release notes
+message = local_repo.tags[release_tag].commit.message #.split("\n",2)[2])
+    
+print("Updating release", release_tag)
+print(message)
+release.update_release(name=release_tag, message=message)
+
+# Fetch artifacts
+args = { 
+    "artifacts": [open("./.ci/artifacts.yml", 'r')],
+    "only_clones": False,
+    "only_artifacts": False,
+    "write_versions": None,
+    "write_env_scripts": None
+}
+os.environ['BF_ARTIFACTS_OVERWRITE_ENDUSER_SOFTWARE_BLICKFELD_SCANNER_LIB'] = release_tag
+fetch_artifacts.fetch_artifacts(namedtuple('Args', args.keys())(*args.values()))
+
+# Delete existing assets
+for asset in release.get_assets():
+    asset.delete_asset()
+
+# Upload assets
+release.upload_asset("build/blickfeld-scanner-lib-dev-Linux.deb")
+release.upload_asset("build/blickfeld-scanner-lib-dev-standalone-Linux.deb")
+release.upload_asset("build/blickfeld-scanner-lib-dev-testing-Linux.deb")
