b326655fc88f4b1f2d15f090d15de2f1533d2bc9
Johannes Meßmer <johannes.messmer@blickfeld.com>
2020-06-08 09:06:03 +0200

    CI: Fix release artifacts upload

--- .ci/upload_release_artifacts.py
+++ .ci/upload_release_artifacts.py
@@ -11,47 +11,56 @@ local_repo = Repo()
 g = Github(os.environ['GITHUB_TOKEN'])
 remote_repo = g.get_repo("Blickfeld/blickfeld-scanner-lib")
 
-# Get latest tag
-release_tag = local_repo.git.describe(abbrev=0)
+# Iterate through all tags
+for tag in local_repo.tags:
+    release_tag = tag.name
 
-# CreateOrFetch release from GitHub API
-try:
-    release = remote_repo.get_release(release_tag)
-except:
-    print("Create release for", release_tag)
-    headers, data = remote_repo._requester.requestJsonAndCheck("POST", remote_repo.url + "/releases", input = {
-        "tag_name": release_tag,
-        "prerelease": True,
-    })
-    
-    release = GitRelease.GitRelease(remote_repo._requester, headers, data, completed=True)
-    
-# Release notes
-message = local_repo.tags[release_tag].commit.message.split("\n",2)[2]
-    
-print("Updating release", release_tag)
-print(message)
-release.update_release(name=release_tag, message=message)
+    # CreateOrFetch release from GitHub API
+    try:
+        release = remote_repo.get_release(release_tag)
+        
+        if release.body:
+            print(f"Skipping {release_tag} as release notes are already set")
+            continue
+    except:
+        print("Create release for", release_tag)
+        headers, data = remote_repo._requester.requestJsonAndCheck("POST", remote_repo.url + "/releases", input = {
+            "tag_name": release_tag,
+            "prerelease": True,
+        })
+        
+        release = GitRelease.GitRelease(remote_repo._requester, headers, data, completed=True)
+        
+    # Release notes
+    message = local_repo.tags[release_tag].commit.message.split("\n",2)[2]
+        
+    print("Updating release", release_tag)
+    print(message)
+    release.update_release(name=release_tag, message=message)
 
-# Fetch artifacts
-args = { 
-    "artifacts": [open("./.ci/artifacts.yml", 'r')],
-    "only_clones": False,
-    "only_artifacts": False,
-    "write_versions": None,
-    "write_env_scripts": None
-}
-os.environ['BF_ARTIFACTS_OVERWRITE_ENDUSER_SOFTWARE_BLICKFELD_SCANNER_LIB'] = release_tag
-fetch_artifacts.fetch_artifacts(namedtuple('Args', args.keys())(*args.values()))
+    try:
+        # Fetch artifacts
+        args = { 
+            "artifacts": [open("./.ci/artifacts.yml", 'r')],
+            "only_clones": False,
+            "only_artifacts": False,
+            "write_versions": None,
+            "write_env_scripts": None
+        }
+        os.environ['BF_ARTIFACTS_OVERWRITE_ENDUSER_SOFTWARE_BLICKFELD_SCANNER_LIB'] = release_tag
+        fetch_artifacts(namedtuple('Args', args.keys())(*args.values()))
+        
+        # Upload package to PyPI
+        os.system(f"twine upload --username __token__ --password {os.environ['PIP_TOKEN']} build_amd64_python/python/dist/blickfeld_scanner-{release_tag[1:]}.tar.gz")
 
-# Delete existing assets
-for asset in release.get_assets():
-    asset.delete_asset()
-
-# Upload assets
-release.upload_asset("build/blickfeld-scanner-lib-dev-Linux.deb")
-release.upload_asset("build/blickfeld-scanner-lib-dev-standalone-Linux.deb")
-release.upload_asset("build/blickfeld-scanner-lib-dev-testing-Linux.deb")
-
-# Upload package to PyPI
-os.system(f"twine upload --repository testpypi --username __token__ --password {os.environ['PIP_TEST_TOKEN']} build_amd64_python/python/dist/blickfeld_scanner.tar.gz")
+        # Delete existing assets
+        for asset in release.get_assets():
+            asset.delete_asset()
+        
+        # Upload assets
+        release.upload_asset("build/blickfeld-scanner-lib-dev-Linux.deb")
+        release.upload_asset("build/blickfeld-scanner-lib-dev-standalone-Linux.deb")
+        release.upload_asset("build/blickfeld-scanner-lib-dev-testing-Linux.deb")
+        release.upload_asset("build_amd64_python/python/dist/blickfeld_scanner.tar.gz", name="python_blickfeld_scanner.tar.gz")   
+    except:
+        print(f"Failed to upload artifacts for {release_tag}.")
